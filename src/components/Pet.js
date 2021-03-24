bfbc6852d9b72fc3832e79f1efc075a5f4b286eb
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-03-24 13:41:49 +0100

    pet comp

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 function Pet({ pet }) {
   const [isVaccinated, setIsVaccinated] = useState(pet.isVaccinated);
+  const [isLoading, setLoading] = useState(false);
+
   function vaccinate() {
     console.log(pet.name, isVaccinated);
+
+    setLoading(true);
     setIsVaccinated(!isVaccinated);
+
     fetch("api/pets", {
       method: "POST",
       headers: {
@@ -14,11 +19,12 @@ function Pet({ pet }) {
     })
       .then((resp) => resp.json().then((data) => console.log(data)))
       .catch((err) => console.log(err));
+    setLoading(false);
   }
   return (
     <div className="Pet">
       {pet.name} - Vaccinated:
-      <button onClick={vaccinate}> {isVaccinated ? "true" : "false"}</button>
+      <button onClick={vaccinate}>{isLoading ? "..." : isVaccinated ? "true" : "false"}</button>
     </div>
   );
 }
