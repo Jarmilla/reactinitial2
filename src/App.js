f26070d5426c38ea5ca863516ef1a828d4fe306e
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-03-24 12:47:03 +0100

    base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import './App.css'
+import "./App.css";
+import React, { useState } from "react";
 
-const App = () => {
+function App() {
+  function getClients() {
+    fetch(`/api/clients?search=${search}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log("Error:", error));
+  }
 
+  const [search, setSearch] = useState("");
+  const [isClickable, setIsClickable] = useState(false);
+
+  function validation(str) {
+    if (str.length >= 3) return setIsClickable(true);
+    else setIsClickable(false);
+  }
   return (
     <div className="App">
-      Todo...
+      <h1>Veterinarian admin - clients</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          validation(e.target.value);
+        }}
+      />
+      <button onClick={() => getClients()} disabled={isClickable ? false : true}>
+        Search
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
